8c01ff8b2305befc8c7f9f716229b4e24cfa768c
Praphul <praphulkumar2023@gmail.com>
2024-11-06 15:30:31 +0530

    Creating: Player Names Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,32 @@
-
 function App() {
-  
-
   return (
-    <h1>React Tic-Tac-Toe</h1>
-  )
+    <main>
+      <div id="game-container">
+        {/* Players */}
+        <ol id="players">
+          <li>
+            <span className="player">
+              <span className="player-name">Player 1</span>
+              <span className="player-icon">X</span>
+            </span>
+
+            <button>Edit</button>
+          </li>
+          <li>
+            <span className="player">
+              <span className="player-name">Player 2</span>
+              <span className="player-icon">O</span>
+            </span>
+            <button>Edit</button>
+          </li>
+        </ol>
+
+        {/* Game Board */}
+      </div>
+
+      {/* Game Logs */}
+    </main>
+  );
 }
 
-export default App
+export default App;
